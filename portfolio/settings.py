c966b6d605264bfb3d9fb192e935cc52bb6e21cf
star1ord <vamsikrishnanori97@gmail.com>
2018-07-13 22:46:32 +0530

    req

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR, '/portfolio/static/')
+os.path.join(BASE_DIR, 'portfolio/static/')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
